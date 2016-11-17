2444e1b7f17dcafa61193e6e89a88dac38c7fe8e
xiaoqiang001 <kang991005214@163.com>
2016-11-17 19:51:58 +0800

    写入修理页面

---
+++ user/js/user.js
@@ -0,0 +1,26 @@
+$.fn.rocket = function() {
+	var $this = this;
+	$(document).scroll(function() {
+		if($("body").scrollTop() < $(window).height() - 500) {
+			$this.fadeOut();
+		} else {
+			$this.fadeIn();
+		}
+	})
+	this.click(function() {
+		$("body").animate({
+			scrollTop: 0
+		})
+	})
+}
+$('.toTop').rocket();
+$.fn.accordion = function(obj) {
+	var open = obj.open;
+	this.children().click(function() {
+		$(this).addClass(open).siblings().removeClass(open);
+		return this;
+	})
+}
+$(".service,.user-hub").accordion({
+	open: "open"
+})
