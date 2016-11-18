fc36b359e63d0411b3b02e2d4889f9334d1c303c
zhangjin187 <326560318@qq.com>
2016-11-18 17:46:33 +0800

    11.18wan

---
+++ user/user/js/user.js
@@ -0,0 +1,32 @@
+$.fn.rocket = function() {
+	var $this = this;
+	$(document).scroll(function() {
+		if($("body").scrollTop() < $(window).height() - 500) {
+			$this.fadeOut();
+		} else {
+			$this.fadeIn();
+		}
+	})
+	this.click(function() {
+		$("body").animate({
+			scrollTop: 0
+		})
+	})
+}
+$('.toTop').rocket();
+$.fn.accordion = function(obj) {
+	var open = obj.open;
+	this.children().click(function() {
+		$(this).addClass(open).siblings().removeClass(open);
+		return this;
+	})
+}
+$(".service,.user-hub").accordion({
+	open: "open"
+})
+$("#dateinfo").jeDate({
+    format:"YYYY-MM-DD hh:mm",
+    isTime:true, 
+    minDate:"2016-09-19 00:00:00",
+    okfun:function(){return false}
+})
